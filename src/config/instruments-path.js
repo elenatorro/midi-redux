2d8c72992f5fd507d85fd10028ab3d2bf38f5413
elenatorro <elenatorro@gmail.com>
2016-12-04 13:33:07 +0100

    ADDED: Instrument path to local server

---
+++ src/config/instruments-path.js
@@ -0,0 +1,10 @@
+export const InstrumentSets = {
+  MUSYNG_KITE: 'MusyngKite',
+  FLUID_R3_GM: 'FluidR3_GM'
+};
+
+export const InstrumentPathConfig = {
+  URL: 'localhost',
+  PORT: 8000,
+  SET: InstrumentSets.FLUID_R3_GM
+};
